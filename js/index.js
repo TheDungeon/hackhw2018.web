dccaea798fcd7a5c684693678d1d5d6043875649
Eli Friedman <elifriedman317@gmail.com>
2018-02-17 20:25:24 -0800

    JS! Centering?

--- js/index.js
+++ js/index.js
@@ -2,22 +2,26 @@
   var config = {
     apiKey: "AIzaSyC2-ypnwEY0R0GhFAxie2UKlBSuJ_5lBqQ",
     authDomain: "hack-hw2018.firebaseapp.com",
-    databaseURL: "https://hack-hw2018.firebaseio.com",
+    databaseURL: "https://hack-hw2018.firebaseio.com/",
     projectId: "hack-hw2018",
     storageBucket: "hack-hw2018.appspot.com",
     messagingSenderId: "28719308696"
   };
   firebase.initializeApp(config);
 }());
-var button = document.getElementById('joinRoom');
+var joinButton = document.getElementById('joinRoom');
 var roomCode = document.getElementById('roomCodeTextInput');
 
-const dataRef = new Firebase ('https://hack-hw2018.firebaseio.com/')
-const itemRef = dataRef.child('Items');
+const dataRef = new Firebase ('https://hack-hw2018.firebaseio.com/%27')
+const playerRef = dataRef.child('Players');
 
-button.addEventListener('click', function() {
-  itemRef.push({
-    item: roomCode.value
+joinButton.addEventListener('click', function() {
+  playerRef.push({
+      name: usernameTextInput.value,
+      isAlive: true
   });
-  roomCode.value = '';
+  usernameTextInput.value = '';
+  roomCodeTextInput.value = '';
+
+  window.location.href = "loading.html";
 })
